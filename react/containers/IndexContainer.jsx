32efad1d48bc415bb53db2443c0dd6bee5a0d173
Saurabh Sood <saurabhsood91@gmail.com>
2017-02-25 23:54:39 -0800

    add more containers

--- react/containers/IndexContainer.jsx
+++ react/containers/IndexContainer.jsx
@@ -1,12 +1,16 @@
 
 import React from "react";
 import Header from "../Header"
+import Sidebar from "../Sidebar"
 
 export default class IndexContainer extends React.Component {
     render() {
         return (
-            <div className="row">
-                <Header className="col-md-8"/>
+            <div>
+                <div className="row">
+                    <Header className="col-md-8 header"/>
+                </div>
+                <Sidebar />
             </div>
         );
     }
